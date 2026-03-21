e8766e33505f1cad5e6d615498715acd41e315dc
RO_MAC <rafa102093@gmail.com>
2026-03-21 12:19:26 -0400

    App: initial swift files.

---
+++ Iphone_app/OGJewelryCapture/OGJewelryCapture/OGJewelryCaptureApp.swift
@@ -0,0 +1,17 @@
+//
+//  OGJewelryCaptureApp.swift
+//  OGJewelryCapture
+//
+//  Created by RAFAEL OSPINO  on 3/21/26.
+//
+
+import SwiftUI
+
+@main
+struct OGJewelryCaptureApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Iphone_app/OGJewelryCapture/OGJewelryCaptureTests/OGJewelryCaptureTests.swift
@@ -0,0 +1,17 @@
+//
+//  OGJewelryCaptureTests.swift
+//  OGJewelryCaptureTests
+//
+//  Created by RAFAEL OSPINO  on 3/21/26.
+//
+
+import Testing
+@testable import OGJewelryCapture
+
+struct OGJewelryCaptureTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
